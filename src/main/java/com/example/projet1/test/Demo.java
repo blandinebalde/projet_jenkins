f5d3ec75d1008714daa0105d95370635a1c827d4
blandine balde <blandinebalde@gmail.com>
2022-05-05 09:30:05 +0000

    Second commit

---
+++ src/main/java/com/example/projet1/test/Demo.java
@@ -0,0 +1,13 @@
+package com.example.projet1.test;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Demo {
+    @GetMapping("/")
+    public String index (){
+       return "Hello World from senegal";
+    }
+}
